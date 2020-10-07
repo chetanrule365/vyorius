e0c91e8ab2997239bbe13c1d393f72d177045747
chetan <chetanrule365@gmail.com>
2020-10-07 12:19:56 +0530

    Updates

---
+++ precache-manifest.11bf5ec1a95e49695a3497a85c05c0d0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0784a94f67788f87070ba6fbfb50381",
+    "url": "/vyorius/index.html"
+  },
+  {
+    "revision": "f64126c5d29101760ba9",
+    "url": "/vyorius/static/css/main.a8b0ac12.chunk.css"
+  },
+  {
+    "revision": "fba98ff7352a6c44b798",
+    "url": "/vyorius/static/js/2.ea92a513.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/vyorius/static/js/2.ea92a513.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f64126c5d29101760ba9",
+    "url": "/vyorius/static/js/main.8f91a9ca.chunk.js"
+  },
+  {
+    "revision": "dbf20f079b05fbcba82f",
+    "url": "/vyorius/static/js/runtime-main.513f1a2a.js"
+  },
+  {
+    "revision": "033e21a869f6163b5364bd1d4ef54f64",
+    "url": "/vyorius/static/media/home.033e21a8.png"
+  }
+]);
