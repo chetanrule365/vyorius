42fbb5ceaa867790a0bbf90fc78c0451f3a8bafb
chetan <chetanrule365@gmail.com>
2020-10-07 12:12:45 +0530

    wrote some media queries

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,22 +3,34 @@ import "../css/header.css";
 function Header() {
   return (
     <header>
-      <ul>
-        <li>
-          <a href="">
+      <div style={{display:"flex", alignItems:"center"}}>
+        <a href="">
           <div className="logo">
-            <img src={require('../assets/logo.png')} alt="" />
-            <img src={require('../assets/logo_text.png')} alt="" />
+            <img src={require("../assets/logo.png")} alt="" />
+            <img src={require("../assets/logo_text.png")} alt="" />
           </div>
-          </a>
-        </li>
-        <li><a href="">Why vyorius?</a></li>
-        <li><a href="">Product</a></li>
-        <li><a href="">Use Cases</a></li>
-        <li><a href="">Team</a></li>
-        <li><a href="">Partners</a></li>
-        <li><a href="">Contact us</a></li>
-      </ul>
+        </a>
+        <ul>
+          <li>
+            <a href="">Why vyorius?</a>
+          </li>
+          <li>
+            <a href="">Product</a>
+          </li>
+          <li>
+            <a href="">Use Cases</a>
+          </li>
+          <li>
+            <a href="">Team</a>
+          </li>
+          <li>
+            <a href="">Partners</a>
+          </li>
+          <li>
+            <a href="">Contact us</a>
+          </li>
+        </ul>
+      </div>
       <button className="primary-button">Launch Vyorius</button>
     </header>
   );
